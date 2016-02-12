c1de3ce0cb58bc20eed42fa2e110f440d0df6480
Daniela Remenska <remenska@gmail.com>
2016-02-12 10:04:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 cd /code
 CONDA_ENVS_PATH=$PWD/envs CONDA_BLD_PATH=$PWD/conda-bld conda build CONDA_PY=$CONDA_PY ./root$ROOT
 chown -R $MYUID .
-anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel dev
+anaconda upload $PWD/conda-bld/*/root-$ROOT*.tar.bz2 --force --channel test
 
